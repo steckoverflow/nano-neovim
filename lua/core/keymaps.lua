95449ac853a3f4b8c68972be4ef4ff0be9131092
Thomas Steck <thomas@steckoverflow.se>
2026-05-05 15:26:10 +0200

    feat: lazygit

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -84,6 +84,31 @@ map("n", "<leader>ap", function()
 	end
 end, { desc = "Toggle Auto Pairs" })
 
+-- GIT
+-- =============================================================================
+map("n", "<leader>gg", function()
+	local buf = vim.api.nvim_create_buf(false, true)
+	local width = math.floor(vim.o.columns * 0.9)
+	local height = math.floor(vim.o.lines * 0.9)
+	local win = vim.api.nvim_open_win(buf, true, {
+		relative = "editor",
+		width = width,
+		height = height,
+		col = math.floor((vim.o.columns - width) / 2),
+		row = math.floor((vim.o.lines - height) / 2),
+		style = "minimal",
+		border = "rounded",
+	})
+	vim.fn.termopen("lazygit", {
+		on_exit = function()
+			if vim.api.nvim_win_is_valid(win) then
+				vim.api.nvim_win_close(win, true)
+			end
+		end,
+	})
+	vim.cmd("startinsert")
+end, { desc = "Lazygit" })
+
 -- FILES & PICKING
 -- =============================================================================
 map("n", "<leader>ff", "<cmd>Pick files<cr>", { desc = "Find files" })
